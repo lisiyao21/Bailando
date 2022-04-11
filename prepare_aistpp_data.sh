8fecf099ce0e94b06d95d5df1258050f4caa5eb6
Li Siyao <81355712+lisiyao21@users.noreply.github.com>
2022-04-11 22:51:29 +0800

    Add files via upload

---
+++ prepare_aistpp_data.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+python _prepro_aistpp.py
+python _prepro_aistpp_music.py
+
+# for actor critic
+python _prepro_aistpp_music.py --store_dir data/aistpp_music_feat --sampling_rate 15360*2
+
+# remove bad dances; the list is from AIST++ project page
+for ff in `cat ignored_list.txt`
+do
+    rm -rf data/aistpp_train_wav_full/$ff
+done
