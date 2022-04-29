76271dc9ce22ab767fbadbe90fa5977132dc952a
lisiyao21 <201621210036@mail.bnu.edu.cn>
2022-04-29 23:03:43 +0800

    uncomment the gt feature computation

--- utils/metrics_new.py
+++ utils/metrics_new.py
@@ -181,7 +181,7 @@ if __name__ == '__main__':
     gt_root = '../data/aist_features_zero_start'
     pred_root = '../experiments/actor_critic/eval/pkl/ep000010'
     print('Calculating and saving features')
-    # calc_and_save_feats(gt_root)
+    calc_and_save_feats(gt_root)
     calc_and_save_feats(pred_root)
 
 
